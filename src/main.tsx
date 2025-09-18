e992f7a8ec666ed84e025a717019e1f6c9b71cf0
gautkoe <gautierkoechlin@gmail.com>
2025-09-18 14:21:07 +0200

    Add basename support to BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
+const BASENAME =
+  import.meta.env.BASE_URL ??
+  (typeof document !== 'undefined' ? new URL(document.baseURI).pathname : '/');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={BASENAME}>
       <App />
     </BrowserRouter>
   </StrictMode>
